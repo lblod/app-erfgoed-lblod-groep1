0045fb06b8a910c07b960853fb5041aeed62ecc6
Riad Abdallah <riad.abdallah@hotmail.com>
2024-09-13 10:14:27 +0200

    Go for a more specific approach

--- scripts/triplify-csv/run.py
+++ scripts/triplify-csv/run.py
@@ -1,32 +1,58 @@
-from triplify_csv.triplify_csv import Rml, CsvOptions
+import csv
+import json
+
+# Load mapping config from JSON file
+with open("config/config.json", "r") as json_file:
+    mapping_rules = json.load(json_file)
+
+
+# Function to transform CSV to Turtle
+def csv_to_turtle(csv_file_path, turtle_file_path):
+    with open(csv_file_path, "r") as csv_file, open(
+        turtle_file_path, "w"
+    ) as turtle_file:
+        reader = csv.DictReader(csv_file, delimiter="\t")
+
+        for row in reader:
+            subject = f"<http://example.com/{row['id']}>"
+            turtle_lines = [f"{subject} rdf:type owl:NamedIndividual ;"]
+
+            for column, value in row.items():
+                if column in mapping_rules:
+                    predicate = mapping_rules[column]["predicate"]
+                    turtle_lines.append(f'    {predicate} "{value}" ;')
+
+            # Remove the last semicolon and add a period
+            turtle_lines[-1] = turtle_lines[-1].rstrip(" ;") + " ."
+            turtle_file.write("\n".join(turtle_lines) + "\n")
+
+
+def parse_csv(file_path):
+    data = []
+
+    with open(file_path, "r") as file:
+        reader = csv.DictReader(file, delimiter="\t")
+        for row in reader:
+            item = {
+                "id": row["id"],
+                "naam": row["naam"],
+                "locatie": row["locatie"],
+                "provincie": row["provincie"],
+                "gemeente": row["gemeente"],
+                "is_vastgesteld": row["is_vastgesteld"],
+                "is_beschermd": row["is_beschermd"],
+                "datering": row["datering"],
+                "gebeurtenissen": row["gebeurtenissen"],
+                "disciplines": row["disciplines"],
+                "dataverantwoordelijke": row["dataverantwoordelijke"],
+            }
+            data.append(item)
+
+    return data
+
 
 def main():
-    # config mapping files are .ttl files
-    configfile = "config/erfgoed-config.ttl"
-
-    # csv files should be .csv files
-    # csvfile1, csvfile2 = 'mycsv1.csv', 'mycsv2.csv'
-    csvfile = "csv/erfgoed.csv"
-
-    # output file must have either a .ttl extension for turtle triples
-    # or a .nq extension for quads
-    outputfile = "erfgoed.ttl"
-
-    rml = Rml()
-
-    # default date format of dates in your CSV files is '%Y-%m-%d'
-    # default csv delimiter is ','
-    # override the defaults by setting options
-    options = CsvOptions(dateformat="%d/%m/%Y", delimiter="	")
-
-    # load one rml and one or more csvs
-    rml.loadFile(configfile, [csvfile], options)
-
-    rml.create_triples()
-
-    # "nquads" for named graphs need a .nq extension
-    # here we are generating triples so .ttl for turtle syntax
-    rml.write_file(outputfile, format="ttl")
+    print("main")
 
 
 if __name__ == "__main__":
