2c53ae1d71714a692c4462a9ba31c5a5bf829d8b
Riad Abdallah <riad.abdallah@hotmail.com>
2024-09-13 11:38:09 +0200

    Add script to normalize virtuoso json output

--- scripts/triplify-csv/scripts/normalize.py
+++ scripts/triplify-csv/scripts/normalize.py
@@ -3,6 +3,23 @@
 
 import json
 
-# Load mapping config from JSON file
-with open("config/config.json", "r") as json_file:
-    mapping_rules = json.load(json_file)
+
+def normalize(file_name):
+    # Load mapping config from JSON file
+    with open(f"preprocessing/{file_name}", "r") as file:
+        data = json.load(file)
+
+    # Transform the city data into the desired dictionary format
+    data_normalized = {item["label"]["value"]: item["s"]["value"] for item in data}
+
+    # Write the transformed mapping to a JSON file
+    with open(f"preprocessing/normalized-{file_name}", "w") as json_file:
+        json.dump(data_normalized, json_file, indent=4)
+
+
+def main():
+    normalize("province.json")
+
+
+if __name__ == "__main__":
+    main()
